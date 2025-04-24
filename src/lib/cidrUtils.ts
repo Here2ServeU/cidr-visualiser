59f964b23278c859ab2e8a1b7f181b03e0876b9d
Here2ServerU <revnaweji@gmail.com>
2025-04-24 18:42:55 -0500

    Updated the cidrUtils.ts script

--- src/lib/cidrUtils.ts
+++ src/lib/cidrUtils.ts
@@ -1,13 +1,13 @@
 import IPCIDR from "ip-cidr";
 
 export function calculateCIDRInfo(cidr: string) {
-  const cidrObj = new IPCIDR(cidr);
-  if (!cidrObj.isValid()) {
+  if (!IPCIDR.isValidCIDR(cidr)) {
     throw new Error("Invalid CIDR");
   }
 
-  const firstUsable = cidrObj.start({ type: 'ip', from: 1 });
-  const lastUsable = cidrObj.end({ type: 'ip', offset: -1 });
+  const cidrObj = new IPCIDR(cidr);
+  const firstUsable = cidrObj.start({ type: "ip", from: 1 });
+  const lastUsable = cidrObj.end({ type: "ip", offset: -1 });
   const subnetMask = cidrObj.subnetMask;
   const count = cidrObj.addressCount - 2;
 
