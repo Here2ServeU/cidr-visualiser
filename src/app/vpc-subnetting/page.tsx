6991098e18ab974462d4ecb827ab876ea540d41b
T2S Student <beginner2master2025@gmail.com>
2025-07-04 14:31:12 -0500

    Updated scripts

---
+++ src/app/vpc-subnetting/page.tsx
@@ -0,0 +1,10 @@
+// src/app/vpc-subnetting/page.tsx
+export default function VPCSubnettingPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">VPC Subnetting Examples</h1>
+      <p>This page shows sample subnetting breakdowns for a /16 VPC.</p>
+      {/* You can add example cards here */}
+    </div>
+  );
+}
